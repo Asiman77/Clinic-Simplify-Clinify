a80af078cdc26c8712ac4a50deef1956d27882bf
Elcinn4013 <elcinquluzade4013@gmail.com>
2026-06-29 22:21:14 +0400

    feat: create LabResponse entity and connect to MedicalRecord

---
+++ src/main/java/az/clinify/demo/entity/LabResponse.java
@@ -0,0 +1,33 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "lab_responses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LabResponse extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "medical_record_id", nullable = false)
+    private MedicalRecord medicalRecord;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "lab_technician_id", nullable = false)
+    private User labTechnician;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String resultText;
+
+    @Column(columnDefinition = "TEXT")
+    private String note;
+}
