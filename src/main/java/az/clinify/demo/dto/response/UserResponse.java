daaec0021a8175f5872509b17af596f0f7eb8d83
revandev11 <abusovrvan34@gmail.com>
2026-06-30 20:52:31 +0400

    feat:UserResponse completed

--- src/main/java/az/clinify/demo/dto/response/UserResponse.java
+++ src/main/java/az/clinify/demo/dto/response/UserResponse.java
@@ -1,4 +1,28 @@
 package az.clinify.demo.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
+    private Long id;
+    private String fin;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private LocalDate birthDate;
+    private String phoneNumber;
+    private String email;
+    private boolean hasAccount;
+    //admin teyin edir
+    private Set<String> roles;
+
 }
