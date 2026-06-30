038412aec1ab63be99a882b702df5be1d57bd975
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-06-30 17:30:59 +0400

    LabResponse and MedicalRecord repos added

--- src/main/java/az/clinify/demo/repository/LabResponseRepository.java
+++ src/main/java/az/clinify/demo/repository/LabResponseRepository.java
@@ -1,4 +1,8 @@
 package az.clinify.demo.repository;
 
-public class LabResponseRepository {
+import az.clinify.demo.entity.LabResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LabResponseRepository extends JpaRepository<LabResponse, Long> {
+
 }
--- src/main/java/az/clinify/demo/repository/MedicalRecordRepository.java
+++ src/main/java/az/clinify/demo/repository/MedicalRecordRepository.java
@@ -1,4 +1,8 @@
 package az.clinify.demo.repository;
 
-public class MedicalRecordRepository {
+import az.clinify.demo.entity.MedicalRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
+
 }
