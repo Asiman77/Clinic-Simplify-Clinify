c3267a4038c1605ff9a9cb46954a3b9a3e2ad335
Sevinc <sevincquliyeva000@gmail.com>
2026-06-30 22:22:59 +0400

    creating generateToken method

---
+++ src/main/java/az/clinify/demo/security/JwtTokenProvider.java
@@ -0,0 +1,45 @@
+package az.clinify.demo.security;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}") // 1 gün
+    private long jwtExpirationInMs;
+
+    private Key getSigningKey() {
+        byte[] keyBytes = this.jwtSecret.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(Authentication authentication, List<String> selectedRoles) {
+        String fin = authentication.getName();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+
+        return Jwts.builder()
+                .setSubject(fin)
+                .claim("roles", selectedRoles) 
+                .setIssuedAt(now) 
+                .setExpiration(expiryDate)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+
+    }
+}
