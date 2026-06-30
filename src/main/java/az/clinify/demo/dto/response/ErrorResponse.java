e9b5988e2845f945d87da33749027185d2bbc7c3
revandev11 <abusovrvan34@gmail.com>
2026-07-01 03:58:34 +0400

    feat:Global exception handler completed

--- src/main/java/az/clinify/demo/dto/response/ErrorResponse.java
+++ src/main/java/az/clinify/demo/dto/response/ErrorResponse.java
@@ -1,19 +1,22 @@
 package az.clinify.demo.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+
 
 @Setter
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ErrorResponse {
     private int status;
     private String error;
     private String message;
-    private LocalDateTime timestamp;
+
+
+
+    public ErrorResponse(int status, String error, String message) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+    }
 }
