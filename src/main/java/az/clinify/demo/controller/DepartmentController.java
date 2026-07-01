e2e08b2625f59e400d9b08f5d22f865cf5efa2db
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-07-01 10:32:35 +0400

    feat : DepartmentController Dependency Injection

--- src/main/java/az/clinify/demo/controller/DepartmentController.java
+++ src/main/java/az/clinify/demo/controller/DepartmentController.java
@@ -1,4 +1,15 @@
 package az.clinify.demo.controller;
 
+
+import az.clinify.demo.service.DepartmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/departments")
 public class DepartmentController {
+
+    public DepartmentService departmentService;
 }
