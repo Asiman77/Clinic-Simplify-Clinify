0017554ad0b812a1e7d90a55c179cdaf5e6e8f75
Elcinn4013 <elcinquluzade4013@gmail.com>
2026-07-01 20:30:49 +0400

    feat: Activate DoctorProfile endpoint added

--- src/main/java/az/clinify/demo/controller/DoctorProfileController.java
+++ src/main/java/az/clinify/demo/controller/DoctorProfileController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -50,5 +51,11 @@ public class DoctorProfileController {
             @PathVariable Long id,
             @Valid @RequestBody UpdateDoctorProfileRequest request) {
         return doctorProfileService.updateDoctor(id, request);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PatchMapping("/{id}/activate")
+    public DoctorProfileResponse activateDoctor(@PathVariable Long id) {
+        return doctorProfileService.activateDoctor(id);
     }
 }
