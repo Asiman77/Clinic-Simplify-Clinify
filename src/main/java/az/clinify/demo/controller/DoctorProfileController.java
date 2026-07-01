053db825806120da79315c71320cb4ceacb6047b
Elcinn4013 <elcinquluzade4013@gmail.com>
2026-07-01 20:03:44 +0400

    feat: Create DoctorProfile endpoint added

--- src/main/java/az/clinify/demo/controller/DoctorProfileController.java
+++ src/main/java/az/clinify/demo/controller/DoctorProfileController.java
@@ -2,13 +2,20 @@ package az.clinify.demo.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import az.clinify.demo.dto.request.CreateDoctorProfileRequest;
 import az.clinify.demo.dto.response.DoctorProfileResponse;
 import az.clinify.demo.service.DoctorProfileService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -25,5 +32,13 @@ public class DoctorProfileController {
     @GetMapping("/{id}")
     public DoctorProfileResponse getDoctorById(@PathVariable Long id) {
         return doctorProfileService.getDoctorById(id);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public DoctorProfileResponse createDoctor(
+            @Valid @RequestBody CreateDoctorProfileRequest request) {
+        return doctorProfileService.createDoctor(request);
     }
 }
