e6c2fc7188ab92180f4912b012b899ce185c6ea1
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-07-01 10:27:58 +0400

    feat: Update Department endpoint

--- src/main/java/az/clinify/demo/service/DepartmentService.java
+++ src/main/java/az/clinify/demo/service/DepartmentService.java
@@ -1,9 +1,11 @@
 package az.clinify.demo.service;
 
 import az.clinify.demo.dto.request.CreateDepartmentRequest;
+import az.clinify.demo.dto.request.UpdateDepartmentRequest;
 import az.clinify.demo.dto.response.DepartmentResponse;
 import az.clinify.demo.entity.Department;
 import az.clinify.demo.exceptions.DepartmentAlreadyExistsException;
+import az.clinify.demo.exceptions.DepartmentNotFoundException;
 import az.clinify.demo.mapper.DepartmentMapper;
 import az.clinify.demo.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
@@ -34,6 +36,30 @@ public class DepartmentService {
         Department savedDepartment = departmentRepository.save(department);
 
         return departmentMapper.toResponse(savedDepartment);
+    }
+
+    @Transactional
+    public DepartmentResponse updateDepartment(Long id , UpdateDepartmentRequest request){
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new DepartmentNotFoundException(id));
+
+        String departmentName = request.getName().trim();
+
+        if(departmentRepository.existsByNameIgnoreCaseAndIdNot(departmentName , id)){
+            throw new DepartmentAlreadyExistsException(departmentName);
+        }
+        department.setName(departmentName);
+        department.setDescription(request.getDescription());
+
+        if(request.getActive() != null ){
+            department.setActive(request.getActive());
+        }
+
+        Department updatedDepartment = departmentRepository.save(department);
+
+        return departmentMapper.toResponse(updatedDepartment);
+
+
     }
 
 }
