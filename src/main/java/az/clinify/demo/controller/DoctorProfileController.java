c6633417b9fea8d7396a674aad6b986f3ce5f61a
Elcinn4013 <elcinquluzade4013@gmail.com>
2026-07-01 20:29:17 +0400

    feat: Update DoctorProfile endpoint added

--- src/main/java/az/clinify/demo/controller/DoctorProfileController.java
+++ src/main/java/az/clinify/demo/controller/DoctorProfileController.java
@@ -7,12 +7,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import az.clinify.demo.dto.request.CreateDoctorProfileRequest;
+import az.clinify.demo.dto.request.UpdateDoctorProfileRequest;
 import az.clinify.demo.dto.response.DoctorProfileResponse;
 import az.clinify.demo.service.DoctorProfileService;
 import jakarta.validation.Valid;
@@ -40,5 +42,13 @@ public class DoctorProfileController {
     public DoctorProfileResponse createDoctor(
             @Valid @RequestBody CreateDoctorProfileRequest request) {
         return doctorProfileService.createDoctor(request);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @PutMapping("/{id}")
+    public DoctorProfileResponse updateDoctor(
+            @PathVariable Long id,
+            @Valid @RequestBody UpdateDoctorProfileRequest request) {
+        return doctorProfileService.updateDoctor(id, request);
     }
 }
