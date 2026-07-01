55cef06d7344274116ff3fc4f8586efa1fc9a460
Sevinc <sevincquliyeva000@gmail.com>
2026-07-01 23:13:28 +0400

    Started working on authService

--- src/main/java/az/clinify/demo/service/AuthService.java
+++ src/main/java/az/clinify/demo/service/AuthService.java
@@ -2,9 +2,36 @@ package az.clinify.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import az.clinify.demo.dto.request.AuthRequestDTO;
+import az.clinify.demo.dto.request.FinCheckRequest;
+import az.clinify.demo.dto.response.AuthResponse;
+import az.clinify.demo.dto.response.FinCheckResponse;
+import az.clinify.demo.entity.Role;
+import az.clinify.demo.entity.User;
+import az.clinify.demo.enums.RoleType;
+import az.clinify.demo.mockServer.MockData;
+import az.clinify.demo.mockServer.MockDataRepository;
+import az.clinify.demo.repository.UserRepository;
+import az.clinify.demo.security.JwtTokenProvider;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.stereotype.Service;
+
 @Service
+@RequiredArgsConstructor
 public class AuthService {
 
-    public 
+    private final UserRepository userRepository;
+    private final MockDataRepository mockDataRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+
+
 
 }
