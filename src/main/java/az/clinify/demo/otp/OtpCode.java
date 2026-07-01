cf4a385b462e1f26fd410d93d890a244de6ee0e4
Sevinc <sevincquliyeva000@gmail.com>
2026-07-01 12:41:12 +0400

    fix: changing name of the class

--- src/main/java/az/clinify/demo/otp/OtpCode.java
+++ src/main/java/az/clinify/demo/otp/OtpCode.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-public class OtpCodeEntity {
+public class OtpCode {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
