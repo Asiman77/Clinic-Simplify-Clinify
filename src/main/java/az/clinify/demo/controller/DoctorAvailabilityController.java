17188bcb816c2a9676b2b55ea1bd7d985467fd1b
revandev11 <abusovrvan34@gmail.com>
2026-07-02 01:04:46 +0400

    feat:all Post and Get methods add to controller

--- src/main/java/az/clinify/demo/controller/DoctorAvailabilityController.java
+++ src/main/java/az/clinify/demo/controller/DoctorAvailabilityController.java
@@ -1,15 +1,42 @@
 package az.clinify.demo.controller;
 
+import az.clinify.demo.dto.request.CreateDoctorAvailabilityRequest;
+import az.clinify.demo.dto.response.DoctorAvailabilityResponse;
 import az.clinify.demo.service.DoctorAvailabilityService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/availabilities")
 @RequiredArgsConstructor
 public class DoctorAvailabilityController {
     private final DoctorAvailabilityService availabilityService;
+
+    @PostMapping
+    public ResponseEntity<DoctorAvailabilityResponse>createAvailability(
+            @Valid @RequestBody CreateDoctorAvailabilityRequest request){
+        DoctorAvailabilityResponse response = availabilityService.createAvailability(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorAvailabilityResponse>getAvailabilityById(@PathVariable Long id){
+        return ResponseEntity.ok(availabilityService.getDoctorAvailabilityById(id));
+    }
+    @GetMapping
+    public ResponseEntity<List<DoctorAvailabilityResponse>>getAllAvailabilities(){
+        return ResponseEntity.ok(availabilityService.getAllDoctorAvailabilities());
+    }
+    @GetMapping("/doctor/{doctorId}")
+    public ResponseEntity<List<DoctorAvailabilityResponse>>getAvailabilitiesByDoctorId(
+            @PathVariable Long doctorId){
+        return ResponseEntity.ok(availabilityService.getDoctorAvailabilitiesByDoctorId(doctorId));
+    }
+
 
 
 }
