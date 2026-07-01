7f2954ccb5db1057bd24dfd7db100d308d4ceec6
Elcinn4013 <elcinquluzade4013@gmail.com>
2026-07-01 13:28:50 +0400

    feat: DoctorProfile helper methods added

--- src/main/java/az/clinify/demo/service/DoctorProfileService.java
+++ src/main/java/az/clinify/demo/service/DoctorProfileService.java
@@ -2,6 +2,11 @@ package az.clinify.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import az.clinify.demo.entity.Department;
+import az.clinify.demo.entity.DoctorProfile;
+import az.clinify.demo.entity.Role;
+import az.clinify.demo.entity.User;
+import az.clinify.demo.enums.RoleType;
 import az.clinify.demo.mapper.DoctorProfileMapper;
 import az.clinify.demo.repository.DepartmentRepository;
 import az.clinify.demo.repository.DoctorProfileRepository;
@@ -17,4 +22,15 @@ public class DoctorProfileService {
     private final DepartmentRepository departmentRepository;
     private final RoleRepository roleRepository;
     private final DoctorProfileMapper doctorProfileMapper;
+
+    private DoctorProfile findDoctorProfileById(Long id) {
+        return doctorProfileRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Doctor profile not found"));
+    }
+
+    private Department findDepartmentById(Long departmentId) {
+        return departmentRepository.findById(departmentId)
+                .orElseThrow(() -> new RuntimeException("Department not found"));
+    }
+
 }
