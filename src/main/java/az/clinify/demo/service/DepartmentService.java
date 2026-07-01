7346eb559e507d8940fb8327a1e201f937713ef2
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-07-01 10:10:04 +0400

    feat : Dependency Injections added to DepartmentService

--- src/main/java/az/clinify/demo/service/DepartmentService.java
+++ src/main/java/az/clinify/demo/service/DepartmentService.java
@@ -1,4 +1,16 @@
 package az.clinify.demo.service;
 
+import az.clinify.demo.mapper.DepartmentMapper;
+import az.clinify.demo.repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+
 public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+    private final DepartmentMapper departmentMapper;
+
 }
