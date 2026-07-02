8f274f8746c300e1773beb03c9d7c97847df9496
revandev11 <abusovrvan34@gmail.com>
2026-07-03 00:42:48 +0400

    createMedicalRecord created

--- src/main/java/az/clinify/demo/controller/MedicalrecordController.java
+++ src/main/java/az/clinify/demo/controller/MedicalrecordController.java
@@ -1,7 +1,14 @@
 package az.clinify.demo.controller;
 
+import az.clinify.demo.dto.request.MedicalRecordRequestDTO;
+import az.clinify.demo.dto.response.MedicalRecordResponseDTO;
 import az.clinify.demo.service.MedicalRecordService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MedicalrecordController {
 private final MedicalRecordService service;
+    @PostMapping
+    public ResponseEntity<MedicalRecordResponseDTO>createMedicalRecord(
+            @Valid @RequestBody MedicalRecordRequestDTO request){
+        MedicalRecordResponseDTO response = service.CreateMedicalRecord(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 
 
 
