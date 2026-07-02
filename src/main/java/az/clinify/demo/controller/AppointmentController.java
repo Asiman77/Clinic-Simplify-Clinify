268cdde0147ddec98c7434471ee17c05d5af33eb
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-02 22:51:50 +0400

    feat: add GET /appointments/patient/{patientId} endpoint

--- src/main/java/az/clinify/demo/controller/AppointmentController.java
+++ src/main/java/az/clinify/demo/controller/AppointmentController.java
@@ -1,9 +1,15 @@
 package az.clinify.demo.controller;
 
+import az.clinify.demo.dto.response.AppointmentResponseDTO;
 import az.clinify.demo.service.AppointmentManagementService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,4 +18,8 @@ public class AppointmentController {
 
     private final AppointmentManagementService appointmentManagementService;
 
+    @GetMapping("/patient/{patientId}")
+    public ResponseEntity<List<AppointmentResponseDTO>> getByPatient(@PathVariable Long patientId) {
+        return ResponseEntity.ok(appointmentManagementService.getByPatient(patientId));
+    }
 }
