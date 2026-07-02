43640fddd23876b85dccbc7cda6f6c1c4a2f0c17
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-07-02 16:31:48 +0400

    Annotasions added to DataLoade

--- src/main/java/az/clinify/demo/configs/DataLoader.java
+++ src/main/java/az/clinify/demo/configs/DataLoader.java
@@ -1,4 +1,17 @@
 package az.clinify.demo.configs;
 
-public class DataLoader {
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+
+public class DataLoader implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
