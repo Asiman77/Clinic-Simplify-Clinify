fa1ad2c7f02694d1d9ddcb0c0f2bd5e0f56a4bdc
Sevinc <sevincquliyeva000@gmail.com>
2026-07-02 15:50:51 +0400

    Adding new dtos for the ath passwd checking

---
+++ src/main/java/az/clinify/demo/dto/request/PasswordSetupRequest.java
@@ -0,0 +1,20 @@
+package az.clinify.demo.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PasswordSetupRequest {
+    @NotBlank(message = "FIN cannot be blank")
+    @Size(min = 7, max = 7, message = "FIN must be exactly 7 characters")
+    private String fin;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
