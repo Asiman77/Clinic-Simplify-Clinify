31483afbfd8f0f837952f19a07c2b172d66ecdfe
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-07-02 16:32:47 +0400

    Dependecies added to class

--- src/main/java/az/clinify/demo/configs/DataLoader.java
+++ src/main/java/az/clinify/demo/configs/DataLoader.java
@@ -1,14 +1,24 @@
 package az.clinify.demo.configs;
 
 
+import az.clinify.demo.repository.RoleRepository;
+import az.clinify.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 
 public class DataLoader implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder passwordEncoder;
+
 
     @Override
     public void run(String... args) throws Exception {
