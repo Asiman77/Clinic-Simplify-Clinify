3951a75844ae1437fd3c4d979bc2646e8de6dd73
Nuray Taghiyeva <tagiyevanuray35@gmail.com>
2026-07-02 21:49:08 +0400

    feat: add findByPatientId method to AppointmentRepository

--- src/main/java/az/clinify/demo/repository/AppointmentRepository.java
+++ src/main/java/az/clinify/demo/repository/AppointmentRepository.java
@@ -14,4 +14,6 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
             LocalDateTime dayStart,
             LocalDateTime dayEnd,
             List<AppointmentStatus> statuses);
+
+    List<Appointment> findByPatientId(Long patientId);
 }
