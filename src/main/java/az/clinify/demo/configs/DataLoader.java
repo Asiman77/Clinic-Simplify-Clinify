9fbe2a0b640515e40ed9dd3c56bedbf3a30adc91
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-07-02 16:34:11 +0400

    Methods implemented

--- src/main/java/az/clinify/demo/configs/DataLoader.java
+++ src/main/java/az/clinify/demo/configs/DataLoader.java
@@ -1,12 +1,15 @@
 package az.clinify.demo.configs;
 
 
+import az.clinify.demo.entity.Role;
+import az.clinify.demo.enums.RoleType;
 import az.clinify.demo.repository.RoleRepository;
 import az.clinify.demo.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -21,7 +24,14 @@ public class DataLoader implements CommandLineRunner {
 
 
     @Override
-    public void run(String... args) throws Exception {
+    @Transactional
+    public void run(String... args) {
+        Role superAdminRole = roleRepository.findByName(RoleType.ADMIN)
+                .orElseGet(() -> {
+                    Role role = new Role();
+                    role.setName(RoleType.ADMIN);
+                    return roleRepository.save(role);
+                });
 
     }
 }
