601138acf943f196416d2d07a330c6f4c5301f85
Sevinc <sevincquliyeva000@gmail.com>
2026-07-03 14:54:34 +0400

    fix: changing query in medicalRecordRepository

--- src/main/java/az/clinify/demo/repository/MedicalRecordRepository.java
+++ src/main/java/az/clinify/demo/repository/MedicalRecordRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Long> {
-    @Query("SELECT new com.example.dto.MedicalRecordSummaryDto(m.id, m.diagnosis, m.labStatus, m.statusUpdatedAt) " +
+    @Query("SELECT new az.clinify.demo.dto.response.MedicalRecordSummaryDto(m.id, m.diagnosis, m.labStatus, m.statusUpdatedAt) " +
             "FROM MedicalRecord m WHERE m.patient.id = :patientId ORDER BY m.recordDate DESC")
     List<MedicalRecordSummaryDto> findAllSummaryByPatientId(@Param("patientId") Long patientId);
 }
