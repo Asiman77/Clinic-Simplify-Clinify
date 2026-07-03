6e298ce170d307f21bac8dfaaf6469986de335f0
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-07-03 18:14:35 +0400

    feat : Annotations added to SwaggerConfig

--- src/main/java/az/clinify/demo/configs/SwaggerConfig.java
+++ src/main/java/az/clinify/demo/configs/SwaggerConfig.java
@@ -1,4 +1,14 @@
 package az.clinify.demo.configs;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
 }
