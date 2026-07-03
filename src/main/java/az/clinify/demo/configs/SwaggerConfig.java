3f2e681f54996fe067b88cd154a5df0456742cbc
Asiman Mirzaliyev <asiman.mirzaliyev2023@khazar.org>
2026-07-03 18:17:18 +0400

    feat : Jwt dependency added

--- src/main/java/az/clinify/demo/configs/SwaggerConfig.java
+++ src/main/java/az/clinify/demo/configs/SwaggerConfig.java
@@ -1,14 +1,25 @@
 package az.clinify.demo.configs;
 
+import az.clinify.demo.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
 }
