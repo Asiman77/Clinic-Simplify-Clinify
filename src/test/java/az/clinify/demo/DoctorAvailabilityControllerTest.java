fb09e5ba4a0341e5ea65a9d1b049ffd6f09103f9
revandev11 <abusovrvan34@gmail.com>
2026-07-07 14:44:52 +0400

    feat:preauthorization annotations added to doctor availability class

--- src/test/java/az/clinify/demo/DoctorAvailabilityControllerTest.java
+++ src/test/java/az/clinify/demo/DoctorAvailabilityControllerTest.java
@@ -1,4 +1,35 @@
 package az.clinify.demo;
 
-public class DoctorAvailabilityControllerTest {
+import az.clinify.demo.configs.SecurityConfig;
+import az.clinify.demo.controller.DoctorAvailabilityController;
+import az.clinify.demo.security.JwtTokenProvider;
+import az.clinify.demo.service.DoctorAvailabilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@WebMvcTest(DoctorAvailabilityController.class)
+@Import({
+        SecurityConfig.class
+})
+class DoctorAvailabilityControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private DoctorAvailabilityService availabilityService;
+
+    @MockitoBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    @MockitoBean
+    private UserDetailsService userDetailsService;
+
+
+
 }
