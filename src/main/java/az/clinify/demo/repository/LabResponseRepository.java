c3dec69522f094ea9e7ff2e841f8aa15154baedd
Elcinn4013 <elcinquluzade4013@gmail.com>
2026-07-13 18:55:47 +0400

    feat: add paginated lab response queries

--- src/main/java/az/clinify/demo/repository/LabResponseRepository.java
+++ src/main/java/az/clinify/demo/repository/LabResponseRepository.java
@@ -2,13 +2,54 @@ package az.clinify.demo.repository;
 
 import az.clinify.demo.entity.LabResponse;
 import az.clinify.demo.enums.LabStatuses;
-
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LabResponseRepository extends JpaRepository<LabResponse, Long> {
-    List<LabResponse> findAllByMedicalRecordId(Long medicalRecordId);
+import java.util.List;
+import java.util.Optional;
 
-    List<LabResponse> findAllByStatus(LabStatuses status);
+public interface LabResponseRepository extends JpaRepository<LabResponse, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = {
+            "medicalRecord",
+            "medicalRecord.patient",
+            "medicalRecord.doctor",
+            "medicalRecord.doctor.user",
+            "labTechnician"
+    })
+    Page<LabResponse> findAll(Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = {
+            "medicalRecord",
+            "medicalRecord.patient",
+            "medicalRecord.doctor",
+            "medicalRecord.doctor.user",
+            "labTechnician"
+    })
+    Optional<LabResponse> findById(Long id);
+
+    @EntityGraph(attributePaths = {
+            "medicalRecord",
+            "medicalRecord.patient",
+            "medicalRecord.doctor",
+            "medicalRecord.doctor.user",
+            "labTechnician"
+    })
+    Page<LabResponse> findAllByStatusIn(
+            List<LabStatuses> statuses,
+            Pageable pageable);
+
+    @EntityGraph(attributePaths = {
+            "medicalRecord",
+            "labTechnician"
+    })
+    List<LabResponse> findAllByMedicalRecordId(
+            Long medicalRecordId);
+
+    List<LabResponse> findAllByStatus(
+            LabStatuses status);
 }
